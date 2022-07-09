6e6868ab8ab8497d8cda0565e66e8d27f6695d37
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2022-07-09 11:39:38 -0300

    feat: implemet paths for authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "signIn"
+  },
+  {
+    path: "signUp"
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
