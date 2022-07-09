96fd0430fd17ba6d07569ba23dece355e91ed797
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2022-07-09 11:17:16 -0300

    feat: add http dependence

--- src/app/services/api/auth.service.ts
+++ src/app/services/api/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient,
+  ) { }
 }
